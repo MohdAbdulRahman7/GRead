9b9919e05b10a099b2d5af220cfb9adde9ba7874
mihir007 <radadiyamihir4061@gmail.com>
2024-07-16 23:42:56 -0400

    Added detailed blog view template with title, body, publication date, and thumbnail image

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,8 @@
-
-
 from django.shortcuts import render
 from .models import Blog
 from django.db.models import Q
-
+from django.http import HttpResponse
+from . import forms
 
 # def blogs_list(request):
 #     blogs = Blog.objects.all().order_by('pub_date')
@@ -30,3 +29,9 @@ def blogs_list(request):
         blogs = Blog.objects.all().order_by('pub_date')
 
     return render(request, 'blogs/blogs_list.html', {'blog_details': blogs, 'query': query, 'field': field})
+
+
+
+def blog_detail(request, slug):
+    blog = Blog.objects.get(slug=slug)
+    return render(request, 'blogs/blog_detail.html', {'blog': blog})
