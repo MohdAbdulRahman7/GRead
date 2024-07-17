0ab7d83ff47ede1fec6564fbd18c40153ff89b1b
MohdAbdulRahman7 <rahman77mohd@gmail.com>
2024-07-17 00:30:26 -0400

    views and url updates -- create

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,3 +1,8 @@
+"""
+URLS - FOR Blogs App
+Urls - call the functions in views.py. Views functions contain logic regd. what to send as response.
+
+"""
 from django.urls import path
 from . import views  # importing views.py from current directory(hence used '.')
 
@@ -5,5 +10,6 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', views.blogs_list, name='blogs_list'),
+    path('create/', views.blog_create, name='blog_create'),
    
 ]
