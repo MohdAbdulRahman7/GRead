2e63e3d9921f765eacbd3b13e84010ff7ac66af8
mihir007 <radadiyamihir4061@gmail.com>
2024-07-17 20:33:05 -0400

    Add detailed blog view: - Add blog_detail view in views.py - Add URL pattern for blog_detail in urls.py - Enhance blog_detail.html with title, body, pub_date, and thumbnail

--- blogs/urls.py
+++ blogs/urls.py
@@ -11,5 +11,7 @@ app_name = 'blogs'
 urlpatterns = [
     path('', views.blogs_list, name='blogs_list'),
     path('create/', views.blog_create, name='blog_create'),
+    # below regex: slug can be any character(word, number etc), also includes'-' and length is nolimit.
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
    
 ]
