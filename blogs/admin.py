cc29c9b39fcb33a460df48f76271463ee5b55765
Meet <meetremotework1234@gmail.com>
2024-07-22 13:42:49 -0400

    blogs details view and template final modifications made as well as recently viewed template

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from .models import Blog, Comment, Like
+from accounts.admin_site import custom_admin_site
 
-from .models import Blog # From models.py file import Blog class
 
-# Register your models here.
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date', 'author', 'status')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('body',)
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'created_at')
+    list_filter = ('post', 'user')
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Like, LikeAdmin)
--- blogs/forms.py
+++ blogs/forms.py
@@ -1,8 +1,27 @@
 from django import forms
 from . import models
+from django.utils.text import slugify
+from .models import Blog, Comment
 
 
 class CreateBlog(forms.ModelForm):
     class Meta:
         model = models.Blog
         fields = ['title', 'body', 'slug', 'thumbnail']
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        slug = slugify(title)
+        if models.Blog.objects.filter(slug=slug).exists():
+            raise forms.ValidationError(
+                "A blog post with a similar title already exists. Please try a different title.")
+        return title
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 4}),
+        }
