35eae9506c1dd829ac82829b775b3e24ddc78e6c
Vaibhav <koratvaibhav123@gmail.com>
2024-07-23 07:28:35 -0400

    Fixes Login and Logout functionalities.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Member
 
-# Register your models here.
+
+class MemberInline(admin.StackedInline):
+    model = Member
+    can_delete = False
+    verbose_name_plural = 'members'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (MemberInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Member)
