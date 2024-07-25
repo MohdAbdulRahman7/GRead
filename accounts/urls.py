3aea8c441860ebf1bd403d86b288a6f067a20c5b
Vaibhav <koratvaibhav123@gmail.com>
2024-07-25 16:43:00 -0400

    Updates Profile functionalities.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,16 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
          name='password_reset_confirm'),
