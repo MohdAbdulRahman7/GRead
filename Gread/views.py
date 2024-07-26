bd8fd6a50d40e598f1487fa69fc45157e69b5e61
Meet <meetremotework1234@gmail.com>
2024-07-26 13:25:23 -0400

    about template and view

--- Gread/views.py
+++ Gread/views.py
@@ -79,17 +79,6 @@ def homepage(request):
 
 
 def about(request):
-    user = get_user(request)
-    cookie_consent = request.COOKIES.get(f'cookie_consent_{user.id}')
-
-    if user.is_authenticated and cookie_consent == 'accepted':
-        user_id = user.id
-        visits_cookie_name = f'visits_{user_id}'
-        visits = int(request.COOKIES.get(visits_cookie_name, '0'))
-        context = {'visits': visits}
-    else:
-        context = {'visits': None}
-
-    return render(request, 'about.html', context)
+    return render(request, 'about.html')
 
 
