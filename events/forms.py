9c069506b4021e338fa717de044a746b241b9b31
mihir007 <radadiyamihir4061@gmail.com>
2024-07-26 00:25:53 -0400

    Added event-form in forms and add event functionality in views

---
+++ events/forms.py
@@ -0,0 +1,12 @@
+# forms.py
+from django import forms
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+    datetime = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+
+    class Meta:
+        model = Event
+        fields = ['title', 'description', 'datetime', 'location', 'enrollment_limit', 'additional_details',
+                  'contact_information', 'event_banner']
--- events/views.py
+++ events/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Event
+from .forms import EventForm
 # Create your views here.
+@login_required
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST, request.FILES)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.author = request.user
+            event.save()
+            return redirect('events:events_list')
+    else:
+        form = EventForm()
+    return render(request, 'events/add_event.html', {'form': form})
